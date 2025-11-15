cae205bb75a246dc3238cab2253fb02157a8869a
delta612 <deltasakawa@gmail.com>
2025-11-15 21:30:27 +0300

    week 5 submission

--- server/config/config.js
+++ server/config/config.js
@@ -3,4 +3,4 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 5000,
-  CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173' // Default Vite port,
+  CLIENT_URL: process.env.CLIENT_URL || 'mongodb+srv://Delta:Delta@cluster0.lmydysu.mongodb.net/?appName=Cluster0' // Default Vite port,
